781304cea3dfc90ca56598bc162a95e060048e9c
Juan Pemberthy <juanp@uber.com>
2014-11-17 18:12:34 -0800

    markdown.el

--- init.el
+++ init.el
@@ -39,6 +39,14 @@
 (eval-after-load 'rspec-mode
   '(rspec-install-snippets))
 
+;; markdown mode
+(add-to-list 'load-path "~/.emacs.d/markdown-mode")
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
 ;; disable soft returns
 (toggle-truncate-lines)
 
