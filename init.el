08da9ecdeb97f0682a046947269b0379b7c0624f
Juan Pemberthy <juanp@uber.com>
2016-07-19 21:39:58 -0700

    Modify go-guru what mode highlight

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(go-guru-hl-identifier-face ((t (:inherit highlight :background "#3e4446")))))
 
 (setq ag-highlight-search t)
 (show-paren-mode 1)
