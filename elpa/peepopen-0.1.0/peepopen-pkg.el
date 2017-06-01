8078baf62d56d716d10ba7d2fee5d2a62706259c
Juan Pemberthy <juanp@uber.com>
2017-05-31 17:10:01 -0700

    May break a bunch of things.

--- elpa/peepopen-0.1.0/peepopen-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "peepopen" "0.1.0" "Graphical file chooser for Emacs on Mac OS X." 'nil)
---
+++ elpa/string-inflection-20161220.1825/string-inflection-pkg.el
@@ -0,0 +1,1 @@
+(define-package "string-inflection" "20161220.1825" "underscore -> UPCASE -> CamelCase -> lowerCamelCase conversion of names" 'nil :keywords '("elisp"))
